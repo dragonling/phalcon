f4c6333a130c3c506186ed4994a6f884cbd6afd6
AlloVince <allo.vince@gmail.com>
2014-03-25 15:06:41 +0800

    fixed name

---
+++ modules/EvaOAuthClient/src/EvaOAuthClient/Entities/AccessTokens.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Eva\EvaOAuthClient\Entities;
+
+class AccessTokens extends \Eva\EvaEngine\Model
+{
+
+    /**
+     *
+     * @var string
+     */
+    public $adapterKey;
+     
+    /**
+     *
+     * @var string
+     */
+    public $token;
+
+     
+    /**
+     *
+     * @var string
+     */
+    public $version;
+     
+    /**
+     *
+     * @var string
+     */
+    public $tokenStatus;
+     
+    /**
+     *
+     * @var string
+     */
+    public $scope;
+     
+    /**
+     *
+     * @var string
+     */
+    public $refreshToken;
+     
+    /**
+     *
+     * @var string
+     */
+    public $refreshTime;
+     
+    /**
+     *
+     * @var string
+     */
+    public $expireTime;
+     
+    /**
+     *
+     * @var string
+     */
+    public $remoteUserId;
+     
+    /**
+     *
+     * @var string
+     */
+    public $remoteUserName;
+     
+    /**
+     *
+     * @var string
+     */
+    public $remoteExtra;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $user_id;
+     
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'adapterKey' => 'adapterKey', 
+            'token' => 'token', 
+            'tokenSecret' => 'tokenSecret', 
+            'version' => 'version', 
+            'tokenStatus' => 'tokenStatus', 
+            'scope' => 'scope', 
+            'refreshToken' => 'refreshToken', 
+            'refreshTime' => 'refreshTime', 
+            'expireTime' => 'expireTime', 
+            'remoteUserId' => 'remoteUserId', 
+            'remoteUserName' => 'remoteUserName', 
+            'remoteExtra' => 'remoteExtra', 
+            'user_id' => 'user_id'
+        );
+    }
+
+    protected $tableName = 'oauth_accesstokens';
+}
