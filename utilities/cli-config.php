ae29c5b8b983d5963b4995a40d76177d1175cd2b
AlloVince <allo.vince@gmail.com>
2014-04-06 16:53:20 +0800

    add doctrine

---
+++ utilities/cli-config.php
@@ -0,0 +1,33 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+require __DIR__ . '/../init_autoloader.php';
+$engine = new \Eva\EvaEngine\Engine(__DIR__ . '/..');
+$engine->loadModules(array(
+    'EvaCore',
+));
+$engine->bootstrap();
+$config = $engine->getDI()->get('config');
+
+$paths = array(__DIR__);
+$isDevMode = true;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => $config->dbAdapter->master->username,
+    'password' => $config->dbAdapter->master->password,
+    'dbname'   => $config->dbAdapter->master->database,
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
+/** @var $em \Doctrine\ORM\EntityManager */
+$platform = $entityManager->getConnection()->getDatabasePlatform();
+$platform->registerDoctrineTypeMapping('enum', 'string');
+
+
+return ConsoleRunner::createHelperSet($entityManager);
