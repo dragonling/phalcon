217c62a1488efae429cfa56f8c1998e03b870843
AlloVince <allo.vince@gmail.com>
2014-05-04 14:31:17 +0800

    update tech api

--- apps/WscnGold/src/WscnGold/Controllers/TechanalysisController.php
+++ apps/WscnGold/src/WscnGold/Controllers/TechanalysisController.php
@@ -7,11 +7,43 @@ use WscnGold\Entities\Techanalysis\Quotes;
 
 class TechanalysisController extends ControllerBase
 {
-    public function indexAction()
+    public function quoteAction()
     {
         $period = $this->dispatcher->getParam('period');
         $period = $period ? $period : '1h';
         $period = in_array($period, array('1m', '5m', '15m', '30m', '1h', '5h', '1d', 'mn')) ? $period : '1h';
+        $symbol = $this->dispatcher->getParam('symbol');
+        $symbol = !$symbol || $symbol == 'index' ? 'XAUUSD' : $symbol;
+        $periodkey =  "period$period";
+        $quote = Quotes::findFirst(array(
+            "columns" => array(
+                "id",
+                "title",
+                $periodkey,
+                'symbol',
+            ),
+            "conditions" => "symbol = :symbol:",
+            "bind"       => array('symbol' => $symbol)
+        ));
+        if(!empty($quote->$periodkey)) {
+            $quote->data = json_decode($quote->$periodkey);
+            unset($quote->$periodkey);
+        }
+        $this->response->setContentType('application/json', 'utf-8');
+        $callback = $this->request->getQuery('callback');
+        if($callback) {
+            $this->response->setContent($callback . '(' . $this->response->getContent() . ')');
+        }
+        return $this->response->setJsonContent($quote);
+    }
+
+
+    public function indexAction()
+    {
+        //$period = $this->dispatcher->getParam('period');
+        //$period = $period ? $period : '1h';
+        //$period = in_array($period, array('1m', '5m', '15m', '30m', '1h', '5h', '1d', 'mn')) ? $period : '1h';
+        $period = '1h';
         $symbol = $this->dispatcher->getParam('symbol');
         $symbol = !$symbol || $symbol == 'index' ? 'XAUUSD' : $symbol;
         $periodkey =  "period$period";
@@ -25,21 +57,26 @@ class TechanalysisController extends ControllerBase
         $this->view->setVar('quotes', $quotes);
 
         $quote = Quotes::findFirst(array(
+            /*
             "columns" => array(
                 "id",
                 "title",
                 $periodkey,
                 'symbol',
             ),
+            */
             "conditions" => "symbol = :symbol:",
             "bind"       => array('symbol' => $symbol)
         ));
         $data = array();
         $this->view->setVar('quote', $quote);
+        $periods = array('period1m', 'period5m', 'period15m', 'period30m', 'period1h', 'period5h', 'period1d', 'periodmn');
+        foreach($periods as $periodkey) {
         if(!empty($quote->$periodkey)) {
-            $data = json_decode($quote->$periodkey, true);
+            $data[$periodkey] = json_decode($quote->$periodkey, true);
         }
-        $this->view->setVar('data', $data);
+        }
+        $this->view->setVar('dataArray', $data);
         /*
         $data = $quote->dump(array(
             'symbol',
