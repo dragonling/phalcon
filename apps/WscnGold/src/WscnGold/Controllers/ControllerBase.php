f685237ab221961b40e0579a0f6bbe30a3351e97
AlloVince <allo.vince@gmail.com>
2014-05-23 14:07:46 +0800

    fixed cache key by query

--- apps/WscnGold/src/WscnGold/Controllers/ControllerBase.php
+++ apps/WscnGold/src/WscnGold/Controllers/ControllerBase.php
@@ -6,8 +6,10 @@ class ControllerBase extends \Eva\EvaEngine\Mvc\Controller\ControllerBase
 {
     public function initialize()
     {
+        $cacheKey = md5($this->request->getURI());
         $this->view->cache(array(
-            'lifetime' => 60
+            'lifetime' => 60,
+            'key' => $cacheKey,
         ));
         $this->view->setModuleLayout('WscnGold', '/views/layouts/default');
         $this->view->setModuleViewsDir('WscnGold', '/views');
